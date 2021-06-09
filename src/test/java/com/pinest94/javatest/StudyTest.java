08febc21e53cff5661da178321feb57dae0c4141
hansol kim <doingnow94@gmail.com>
2021-06-10 01:33:58 +0900

    Java Test Project Setting & JUnit5 Annotation code writing

--- src/test/java/com/pinest94/javatest/StudyTest.java
+++ src/test/java/com/pinest94/javatest/StudyTest.java
@@ -1,7 +1,44 @@
 package com.pinest94.javatest;
 
+import org.junit.jupiter.api.*;
+import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
+
+import javax.xml.ws.WebEndpoint;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudyTest {
-    
+
+    @Test
+    void create1() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create1");
+    }
+
+    @Test
+    @Disabled
+    void create2() {
+        System.out.println("create2");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each");
+    }
 }
